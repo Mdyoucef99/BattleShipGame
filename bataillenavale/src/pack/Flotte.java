910a168212cbb1b2c36841617a8c95b75db32e53
radaleng <58236617+radaleng@users.noreply.github.com>
2021-10-24 22:07:02 -0400

    Add files via upload

--- bataillenavale/src/pack/Flotte.java
+++ bataillenavale/src/pack/Flotte.java
@@ -1,27 +1,10 @@
-package pack;
-
 import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Flotte {
 	
-	List<Navire> Navirelist = new ArrayList<Navire>();
-	
-	Coord cord1 = new Coord(1,5);
-	Coord cord2 = new Coord(1,8);
-	
-	Coord cord3 = new Coord(1,4);
-	Coord cord4 = new Coord(2,4);
-	
-	Coord cord5 = new Coord(3,1);
-	Coord cord6 = new Coord(3,5);
-	
-	Coord cord7 = new Coord(8,5);
-	Coord cord8 = new Coord(8,6);
-	
-	Coord cord9 = new Coord(4,9);
-	Coord cord10 = new Coord(7,9);
+	List<Navire> list = new ArrayList<Navire>();
 	
 	public Flotte() 
 	{
@@ -29,40 +12,100 @@ public class Flotte {
 		
 	}
 	
+	 public Navire[] getTabNavires() {
+		 Navire[] arrayBateau = (Navire[]) list.toArray();
+		 return arrayBateau;
+	 }
+	
 	public boolean dejaRecuCoup(Coord tir) 
 	{
+		boolean dejaRecuCoup = false;
+		for(Navire Navire : list) {
+			if(Navire.tirAtouche(tir) == true){
+
+				
+				dejaRecuCoup = true;
+			}
+		}
 		
-		return false;
+		
+		
+		
+		return dejaRecuCoup;
 	}
 	
 	public boolean jeuTermine() 
 	{
+
+		boolean jeuTermine = false;
+
+		int taille = 0;
+
+		for(Navire Navire : list) {
+			if(Navire.estCoule() == true) {
+				
+				taille++;
+				if(taille == list.size()) {
+					
+					jeuTermine = true;
+				}
+			}
+
+		}
 		
-		return false;
+
+		return jeuTermine;
 	}
 	
 	
 	public boolean leTirTouche(Coord tir) 
 	{
+		boolean leTirTouche = false;
+		for(Navire Navire : list) {
+			if(Navire.tirAtouche(tir) == true){
+
+				
+				leTirTouche = true;
+			}
+		}
 		
-		return false;
+		
+		
+		
+		return leTirTouche;
 	}
 	
 	
 	public void InstancierFlotte()  
 	{
+		Coord cord1 = new Coord(1,5);
+		Coord cord2 = new Coord(1,8);
+		
+		Coord cord3 = new Coord(1,4);
+		Coord cord4 = new Coord(2,4);
+		
+		Coord cord5 = new Coord(3,1);
+		Coord cord6 = new Coord(3,5);
+		
+		Coord cord7 = new Coord(8,5);
+		Coord cord8 = new Coord(8,6);
+		
+		Coord cord9 = new Coord(4,9);
+		Coord cord10 = new Coord(7,9);
 		
 		Navire porteavions = new Navire(Constantes.PORTE_AVION,cord1,cord2,Color.RED);
-		Navirelist.add(porteavions);
+		list.add(porteavions);
 		Navire croiseur = new Navire(Constantes.CROISEUR,cord3,cord4,Color.BLUE);
-		Navirelist.add(croiseur);
+		list.add(croiseur);
 		Navire contretorpilleurs = new Navire(Constantes.DESTROYER,cord5,cord6,Color.YELLOW);
-		Navirelist.add(contretorpilleurs);
+		list.add(contretorpilleurs);
 		Navire sousmarin = new Navire(Constantes.SOUS_MARIN,cord7,cord8,Color.BLACK);
-		Navirelist.add(sousmarin);
+		list.add(sousmarin);
 		Navire torpilleur = new Navire(Constantes.CUIRASSE,cord9,cord10,Color.ORANGE);
-		Navirelist.add(torpilleur);
-	
+		list.add(torpilleur);
+
+		
+		
 	}
 	
 	
